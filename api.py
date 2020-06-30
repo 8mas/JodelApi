ce768a3508c96b58953c8f17a78c377560d2c62c
see-aestas <51927781+see-aestas@users.noreply.github.com>
2020-06-30 18:51:45 +0200

    add requests for an account (access token) requests and hmac are valid

--- api.py
+++ api.py
@@ -4,12 +4,14 @@ import time
 import requests
 import secrets
 import hmac
+import json
 
 
 class API:
     debug = True
     instances = 0
-    api_version = '0.2'
+    URL = "https://api.go-tellm.com"
+    api_version = "0.2"
 
     # Android - found via reverse engineering
     secret = secrets.api_secret
@@ -17,10 +19,10 @@ class API:
     client_id_android = "81e8a76e-1e02-4d17-9ba0-8a7020261b26"
 
     # Version - secret is bound to this / see playstore for version
-    client_type = 'android_5.88.1'
+    client_type = "android_5.88.1"
 
     def __init__(self, latitude=53, longitude=9, iid=None, device_id=None, client_id=client_id_android, city="Hamburg",
-                 country="DE", loc_accuracy="5.14", language="en-US", token=None):
+                 country="DE", loc_accuracy=7.41, language="en-US", token=None):
 
         self.request_session = requests.session()
         self.request_session.verify = False
@@ -28,14 +30,14 @@ class API:
         # Use local proxy
         if API.debug:
             print("Using proxy")
-            self.request_session.proxies.update({'http': 'http://127.0.0.1:8888', 'https': 'https://127.0.0.1:8888', })
+            self.request_session.proxies.update({"http": "http://127.0.0.1:8888", "https": "https://127.0.0.1:8888", })
 
         # Randomize the position a bit (up to one km in each direction)
         lat_seed = float(random.randint(-100000, 100000)) / 10000000
         long_seed = float(random.randint(-100000, 100000)) / 10000000
 
-        self.latitude = str(latitude + lat_seed)
-        self.longitude = str(longitude + long_seed)
+        self.latitude = latitude + lat_seed
+        self.longitude = longitude + long_seed
 
         self.city = city
         self.country = country
@@ -45,23 +47,31 @@ class API:
 
         if iid is None:
             self.iid = API._get_random_iid()
+        else:
+            self.iid = iid
+
         if device_id is None:
             self.device_id = API._get_random_device_id()
-        if token is None:
-            pass
-            # TODO generate new user
+        else:
+            self.device_id = device_id
 
-        self.access_token = token
+        if token is None:
+            self.access_token = self.get_access_token()
+        else:
+            self.access_token = token
+
         API.instances += 1
 
     def get_access_token(self):
+        # Get new access token
+        # TODO: handling of renew token etc.
         payload = {
             "location": {
                 "city": self.city,
                 "country": self.country,
                 "loc_coordinates": {
-                    "lat": self.latitude,
-                    "lon": self.longitude
+                    "lng": self.latitude,
+                    "lat": self.longitude
                 },
                 "loc_accuracy": self.loc_accuracy
             },
@@ -70,24 +80,51 @@ class API:
             "device_uid": self.device_id,
             "language": self.language
         }
+        json_string = json.dumps(payload)
+        response = self._post("/api/v2/users", json_string)
 
-    def _generate_hmac(self, type, url, timestamp, data):
-        resource_with_parameters = url.split("https://api.go-tellm.com")[1]
-        resource = resource_with_parameters.split("?")[0]
+        return response["access_token"]
 
+    def _generate_hmac(self, request_type, resource, timestamp, data: str):
         # This format is how jodel needs it. Some requests do not need a valid mac.
-        payload = f"{type}%api.go-tellm.com%443%{resource}%%{self.latitude};{self.longitude}%{timestamp}%%{data}"
+        payload = f"{request_type}%api.go-tellm.com%443%{resource}%%{self.latitude};{self.longitude}%{timestamp}%%{data}"
 
         auth_code = hmac.new(self.secret,
                              payload.encode("utf-8"),
                              hashlib.sha1).hexdigest().upper()
         return auth_code
 
+    # Set headers for request
+    def _prepare_request(self, request_type, resource, data):
+        timestamp = API._get_timestamp()
+        mac = self._generate_hmac(request_type, resource, timestamp, data)
+
+        header_dict = {
+            "X-Timestamp": timestamp,
+            "X-Location": f"{self.latitude};{self.longitude}",
+            "X-Authorization": f"HMAC {mac}",
+            "X-Api-Version": API.api_version,
+            "X-Client-Type": self.client_type,
+            "User-Agent": "Jodel/4.55 (iPad; iOS 11.1; Scale/2.10)",
+            "Content-Type": "application/json"
+        }
+
+        self.request_session.headers.update(header_dict)
+
+    # TODO: Response handling: Errors etc
+    def _handle_response(self, response):
+        print(response.content)
+        return response
+
     def _get(self, url, **kwargs):
         pass
 
-    def _post(self):
-        pass
+    def _post(self, resource, payload: str):
+        url = API.URL + resource
+
+        self._prepare_request("POST", resource, payload)
+        response = self.request_session.post(url, payload)
+        return self._handle_response(response)
 
     def _put(self):
         pass
@@ -100,7 +137,7 @@ class API:
 
     @staticmethod
     def _get_random_hex(n):
-        return ''.join([random.choice('0123456789ABCDEF') for x in range(n)]).lower()
+        return "".join([random.choice("0123456789ABCDEF") for x in range(n)]).lower()
 
     @staticmethod
     def _get_random_device_id():
@@ -116,9 +153,17 @@ class API:
 
     @staticmethod
     def _get_random_AZaz09(n, extra_chars=""):
-        return ''.join(
-            [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' + extra_chars) for _ in
+        return "".join(
+            [random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" + extra_chars) for _ in
              range(n)])
 
+    @staticmethod
+    def _get_timestamp():
+        return time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
     def _log(self, msg):
-        print('[%s]\n%s\n' % (time.strftime('%H:%M:%S'), msg.encode('utf-8')))
+        print("[%s]\n%s\n" % (time.strftime("%H:%M:%S"), msg.encode("utf-8")))
+
+
+if __name__ == "__main__":
+    API()
